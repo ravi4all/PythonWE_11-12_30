4b3acbe7ecec610ee9e900680747f8f8daefb521
ravi4all <ravikanttyagi.2295@gmail.com>
2017-10-01 12:38:50 +0530

    Add files via upload

---
+++ CorePython/02-Loops/01-ForLoop.py
@@ -0,0 +1,11 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(10,20):
+#     print(i, end='-----')
+
+# for i in range(1,11):
+#     print(i*3)
+
+for i in range(3,31,3):
+    print(i)
---
+++ CorePython/02-Loops/02-NestedLoops.py
@@ -0,0 +1,9 @@
+# for i in range(10):
+#     for j in range(i):
+#         print(j, end=' ')
+#     print()
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print()
