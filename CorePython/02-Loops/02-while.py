81bdaf6808906cc617d5fa781b441ec41c505574
ravi4all <ravikanttyagi.2295@gmail.com>
2017-10-05 16:17:53 +0530

    Add files via upload

---
+++ CorePython/02-Loops/02-while.py
@@ -0,0 +1,10 @@
+a = 10
+
+loop = True
+
+while loop:
+    print("Value of a is {}".format(a))
+    a = a - 1
+    if a == 0:
+        break
+
---
+++ CorePython/02-Loops/03-BreakContinue.py
@@ -0,0 +1,5 @@
+for i in range(20):
+    print(i)
+    if i%2 == 0:
+        print("Inside Continue")
+        continue
