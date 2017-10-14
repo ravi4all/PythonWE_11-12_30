ae398b0f66a072d41e4524d05c52a01a199f544d
ravi4all <ravikanttyagi.2295@gmail.com>
2017-10-14 12:48:13 +0530

    Add files via upload

---
+++ Programs/02-Patterns.py
@@ -0,0 +1,10 @@
+# for i in range(1,6):
+#     print("*" * i)
+
+# for i in reversed(range(1,6)):
+#     print("*" * i)
+
+for i in range(1,7):
+    for j in range(1,i+1):
+        print(i, end="")
+    print()
